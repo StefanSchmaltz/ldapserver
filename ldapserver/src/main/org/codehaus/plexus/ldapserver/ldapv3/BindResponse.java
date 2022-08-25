92d09164e9ba4190e438897052b60221b250edf5
Stefan.Schmaltz <32697844+StefanSchmaltz@users.noreply.github.com>
2022-08-25 11:55:46 +0200

    Adjusted BindResponse and SeqList

--- ldapserver/src/main/org/codehaus/plexus/ldapserver/ldapv3/BindResponse.java
+++ ldapserver/src/main/org/codehaus/plexus/ldapserver/ldapv3/BindResponse.java
@@ -1,5 +1,7 @@
 package org.codehaus.plexus.ldapserver.ldapv3;
 
+import java.io.PrintStream;
+
 //-----------------------------------------------------------------------------
 //   NOTE: this is a machine generated file - editing not recommended
 //
@@ -12,11 +14,11 @@ package org.codehaus.plexus.ldapserver.ldapv3;
 //-----------------------------------------------------------------------------
 
 // Import PrintStream class for print methods
-
-import com.ibm.asn1.*;
-import com.ibm.util.*;
-
-import java.io.PrintStream;
+import com.ibm.asn1.ASN1Decoder;
+import com.ibm.asn1.ASN1Encoder;
+import com.ibm.asn1.ASN1Exception;
+import com.ibm.asn1.ASN1Tag;
+import com.ibm.util.HexOutputStream;
 
 /** This class represents the ASN.1 SEQUENCE type <tt>BindResponse</tt>.
  * For each sequence member, sequence classes contain a
@@ -29,159 +31,161 @@ import java.io.PrintStream;
 public class BindResponse implements LDAPv3
 {
 
-    /** member variable representing the sequence member resultCode of type LDAPResultEnum */
-    public LDAPResultEnum resultCode = new LDAPResultEnum();
-    /** member variable representing the sequence member matchedDN of type byte[] */
-    public byte[] matchedDN;
-    /** member variable representing the sequence member errorMessage of type byte[] */
-    public byte[] errorMessage;
-    /** member variable representing the sequence member referral of type Referral */
-    public Referral referral = null;
-    /** member variable representing the sequence member serverSaslCreds of type byte[] */
-    public byte[] serverSaslCreds = null;
+  /** member variable representing the sequence member resultCode of type LDAPResultEnum */
+  public LDAPResultEnum resultCode = new LDAPResultEnum();
+  /** member variable representing the sequence member matchedDN of type byte[] */
+  public byte[] matchedDN;
+  /** member variable representing the sequence member errorMessage of type byte[] */
+  public byte[] errorMessage;
+  /** member variable representing the sequence member referral of type Referral */
+  public Referral referral = null;
+  /** member variable representing the sequence member serverSaslCreds of type byte[] */
+  public byte[] serverSaslCreds = null;
 
-    /** default constructor */
-    public BindResponse()
+  /** default constructor */
+  public BindResponse()
+  {
+  }
+
+  /** copy constructor */
+  public BindResponse( BindResponse arg )
+  {
+    resultCode = new LDAPResultEnum( arg.resultCode );
+    matchedDN = new byte[arg.matchedDN.length];
+    System.arraycopy( arg.matchedDN, 0, matchedDN, 0, arg.matchedDN.length );
+    errorMessage = new byte[arg.errorMessage.length];
+    System.arraycopy( arg.errorMessage, 0, errorMessage, 0, arg.errorMessage.length );
+    referral = new Referral( arg.referral );
+    serverSaslCreds = new byte[arg.serverSaslCreds.length];
+    System.arraycopy( arg.serverSaslCreds, 0, serverSaslCreds, 0, arg.serverSaslCreds.length );
+  }
+
+  /** decoding method.
+   * @param dec
+   *        decoder object derived from com.ibm.asn1.ASN1Decoder
+   * @exception com.ibm.asn1.ASN1Exception
+   *            decoding error
+   */
+  @Override
+  public void decode( ASN1Decoder dec ) throws ASN1Exception
+  {
+    dec.nextIsImplicit( ASN1Tag.makeTag( ASN1Tag.APPLICATION_TAG_CLASS, 1 ) );
+    int seq_nr = dec.decodeSequence();
+    resultCode.decode( dec );
+    matchedDN = dec.decodeOctetString();
+    errorMessage = dec.decodeOctetString();
+    if ( !dec.nextIsOptional( ASN1Tag.makeTag( ASN1Tag.CONTEXT_TAG_CLASS, 3 ) ) )
     {
+      referral = new Referral();
+      dec.nextIsImplicit( ASN1Tag.makeTag( ASN1Tag.CONTEXT_TAG_CLASS, 3 ) );
+      referral.decode( dec );
+    }
+    if ( !dec.nextIsOptional( ASN1Tag.makeTag( ASN1Tag.CONTEXT_TAG_CLASS, 7 ) ) )
+    {
+      dec.nextIsImplicit( ASN1Tag.makeTag( ASN1Tag.CONTEXT_TAG_CLASS, 7 ) );
+      serverSaslCreds = dec.decodeOctetString();
+    }
+    dec.endOf( seq_nr );
+  }
+
+  /** encoding method.
+   * @param enc
+   *        encoder object derived from com.ibm.asn1.ASN1Encoder
+   * @exception com.ibm.asn1.ASN1Exception
+   *            encoding error
+   */
+  @Override
+  public void encode( ASN1Encoder enc ) throws ASN1Exception
+  {
+    enc.nextIsImplicit( ASN1Tag.makeTag( ASN1Tag.APPLICATION_TAG_CLASS, 1 ) );
+    int seq_nr = enc.encodeSequence();
+    resultCode.encode( enc );
+    enc.encodeOctetString( matchedDN );
+    enc.encodeOctetString( errorMessage );
+    if ( referral != null )
+    {
+      enc.nextIsImplicit( ASN1Tag.makeTag( ASN1Tag.CONTEXT_TAG_CLASS, 3 ) );
+      referral.encode( enc );
+    }
+    if (serverSaslCreds != null)
+    {
+      enc.nextIsImplicit( ASN1Tag.makeTag( ASN1Tag.CONTEXT_TAG_CLASS, 7 ) );
+      enc.encodeOctetString( serverSaslCreds );
+    }
+    enc.endOf( seq_nr );
+  }
+
+  /** default print method (variable indentation)
+   * @param os
+   *        PrintStream representing the print destination (file, etc)
+   */
+  public void print( PrintStream os )
+  {
+    print( os, 0 );
+  }
+
+  /** print method (variable indentation)
+   * @param os
+   *        PrintStream representing the print destination (file, etc)
+   * @param indent
+   *        number of blanks that preceed each output line.
+   */
+  public void print( PrintStream os, int indent )
+  {
+    os.println( "{ -- SEQUENCE --" );
+    for ( int ii = 0; ii < indent + 2; ii++ ) os.print( ' ' );
+    os.print( "resultCode = " );
+    resultCode.print( os, indent + 2 );
+    os.println( ',' );
+
+    for ( int ii = 0; ii < indent + 2; ii++ ) os.print( ' ' );
+    os.print( "matchedDN = " );
+    try
+    {
+      ( new HexOutputStream( os ) ).write( matchedDN );
+    }
+    catch ( java.io.IOException ex )
+    {
+      os.print( "( unprintable OCTET STRING value )" );
+    }
+    os.println( ',' );
+
+    for ( int ii = 0; ii < indent + 2; ii++ ) os.print( ' ' );
+    os.print( "errorMessage = " );
+    try
+    {
+      ( new HexOutputStream( os ) ).write( errorMessage );
+    }
+    catch ( java.io.IOException ex )
+    {
+      os.print( "( unprintable OCTET STRING value )" );
     }
 
-    /** copy constructor */
-    public BindResponse( BindResponse arg )
+    os.println( ',' );
+    if ( referral != null )
     {
-        resultCode = new LDAPResultEnum( arg.resultCode );
-        matchedDN = new byte[arg.matchedDN.length];
-        System.arraycopy( arg.matchedDN, 0, matchedDN, 0, arg.matchedDN.length );
-        errorMessage = new byte[arg.errorMessage.length];
-        System.arraycopy( arg.errorMessage, 0, errorMessage, 0, arg.errorMessage.length );
-        referral = new Referral( arg.referral );
-        serverSaslCreds = new byte[arg.serverSaslCreds.length];
-        System.arraycopy( arg.serverSaslCreds, 0, serverSaslCreds, 0, arg.serverSaslCreds.length );
+      for ( int ii = 0; ii < indent + 2; ii++ ) os.print( ' ' );
+      os.print( "referral = " );
+      referral.print( os, indent + 2 );
     }
 
-    /** decoding method.
-     * @param dec
-     *        decoder object derived from com.ibm.asn1.ASN1Decoder
-     * @exception com.ibm.asn1.ASN1Exception
-     *            decoding error
-     */
-    public void decode( ASN1Decoder dec ) throws ASN1Exception
+    os.println( ',' );
+    if ( serverSaslCreds != null )
     {
-        dec.nextIsImplicit( dec.makeTag( dec.APPLICATION_TAG_CLASS, 1 ) );
-        int seq_nr = dec.decodeSequence();
-        resultCode.decode( dec );
-        matchedDN = dec.decodeOctetString();
-        errorMessage = dec.decodeOctetString();
-        if ( !dec.nextIsOptional( dec.makeTag( dec.CONTEXT_TAG_CLASS, 3 ) ) )
-        {
-            referral = new Referral();
-            dec.nextIsImplicit( dec.makeTag( dec.CONTEXT_TAG_CLASS, 3 ) );
-            referral.decode( dec );
-        }
-        if ( !dec.nextIsOptional( dec.makeTag( dec.CONTEXT_TAG_CLASS, 7 ) ) )
-        {
-            dec.nextIsImplicit( dec.makeTag( dec.CONTEXT_TAG_CLASS, 7 ) );
-            serverSaslCreds = dec.decodeOctetString();
-        }
-        dec.endOf( seq_nr );
+      for ( int ii = 0; ii < indent + 2; ii++ ) os.print( ' ' );
+      os.print( "serverSaslCreds = " );
+      try
+      {
+        ( new HexOutputStream( os ) ).write( serverSaslCreds );
+      }
+      catch ( java.io.IOException ex )
+      {
+        os.print( "( unprintable OCTET STRING value )" );
+      }
     }
 
-    /** encoding method.
-     * @param enc
-     *        encoder object derived from com.ibm.asn1.ASN1Encoder
-     * @exception com.ibm.asn1.ASN1Exception
-     *            encoding error
-     */
-    public void encode( ASN1Encoder enc ) throws ASN1Exception
-    {
-        enc.nextIsImplicit( enc.makeTag( enc.APPLICATION_TAG_CLASS, 1 ) );
-        int seq_nr = enc.encodeSequence();
-        resultCode.encode( enc );
-        enc.encodeOctetString( matchedDN );
-        enc.encodeOctetString( errorMessage );
-        if ( referral != null )
-        {
-            enc.nextIsImplicit( enc.makeTag( enc.CONTEXT_TAG_CLASS, 3 ) );
-            referral.encode( enc );
-        }
-        if ( serverSaslCreds != null )
-        {
-            enc.nextIsImplicit( enc.makeTag( enc.CONTEXT_TAG_CLASS, 7 ) );
-            enc.encodeOctetString( serverSaslCreds );
-        }
-        enc.endOf( seq_nr );
-    }
-
-    /** default print method (variable indentation)
-     * @param os
-     *        PrintStream representing the print destination (file, etc)
-     */
-    public void print( PrintStream os )
-    {
-        print( os, 0 );
-    }
-
-    /** print method (variable indentation)
-     * @param os
-     *        PrintStream representing the print destination (file, etc)
-     * @param indent
-     *        number of blanks that preceed each output line.
-     */
-    public void print( PrintStream os, int indent )
-    {
-        os.println( "{ -- SEQUENCE --" );
-        for ( int ii = 0; ii < indent + 2; ii++ ) os.print( ' ' );
-        os.print( "resultCode = " );
-        resultCode.print( os, indent + 2 );
-        os.println( ',' );
-
-        for ( int ii = 0; ii < indent + 2; ii++ ) os.print( ' ' );
-        os.print( "matchedDN = " );
-        try
-        {
-            ( new HexOutputStream( os ) ).write( matchedDN );
-        }
-        catch ( java.io.IOException ex )
-        {
-            os.print( "( unprintable OCTET STRING value )" );
-        }
-        os.println( ',' );
-
-        for ( int ii = 0; ii < indent + 2; ii++ ) os.print( ' ' );
-        os.print( "errorMessage = " );
-        try
-        {
-            ( new HexOutputStream( os ) ).write( errorMessage );
-        }
-        catch ( java.io.IOException ex )
-        {
-            os.print( "( unprintable OCTET STRING value )" );
-        }
-
-        os.println( ',' );
-        if ( referral != null )
-        {
-            for ( int ii = 0; ii < indent + 2; ii++ ) os.print( ' ' );
-            os.print( "referral = " );
-            referral.print( os, indent + 2 );
-        }
-
-        os.println( ',' );
-        if ( serverSaslCreds != null )
-        {
-            for ( int ii = 0; ii < indent + 2; ii++ ) os.print( ' ' );
-            os.print( "serverSaslCreds = " );
-            try
-            {
-                ( new HexOutputStream( os ) ).write( serverSaslCreds );
-            }
-            catch ( java.io.IOException ex )
-            {
-                os.print( "( unprintable OCTET STRING value )" );
-            }
-        }
-
-        os.println();
-        for ( int ii = 0; ii < indent; ii++ ) os.print( ' ' );
-        os.print( '}' );
-    }
+    os.println();
+    for ( int ii = 0; ii < indent; ii++ ) os.print( ' ' );
+    os.print( '}' );
+  }
 }
